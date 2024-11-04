e0cbbd5f9c3756e697949d4867b8efd089090305
Diego Lauar <diego_pktd@hotmail.com>
2024-11-04 15:03:52 -0300

    add hash passowrd

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
-import fastify from "fastify";
+import fastify from 'fastify'
+import { appRoutes } from './http/routes'
 
 export const app = fastify()
+
+console.log('a')
+
+app.register(appRoutes)
+
+
+
+//CRIAR IMAGEM DOCKER EXEMPLO BACNO:
+// docker run --name api-solid-pg -e POSTGRESQL_USERNAME=docker -e POSTGRESQL_PASSWORD=docker -e POSTGRESQL_DATABASE=apisolid -p 5432:5432 bitnami/postgresql
