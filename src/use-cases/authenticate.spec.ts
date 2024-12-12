4933726843786bdb2f539fe1f720061312a9e32b
Diego Lauar <diego_pktd@hotmail.com>
2024-12-12 17:05:50 -0300

    Add instancia antes dos testes

--- src/use-cases/authenticate.spec.ts
+++ src/use-cases/authenticate.spec.ts
@@ -2,54 +2,55 @@ import { InMemoryUsersRepository } from './../repositories/in-memory/in-memory-u
 import { AuthenticateUseCase } from './../use-cases/authenticate'
 import { InvalidCredentialsError } from './../use-cases/errors/invalid-credentials-error'
 import { hash } from 'bcryptjs'
-import { expect, describe, it } from 'vitest'
+import { expect, describe, it, beforeEach } from 'vitest'
+
+
+let usersRepository: InMemoryUsersRepository
+let sut: AuthenticateUseCase
+
+beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository()
+    sut = new AuthenticateUseCase(usersRepository)
+
+})
 
 describe('Authenticate Use Case', () => {
-  it('should be able to authenticate', async () => {
-    const usersRepository = new InMemoryUsersRepository()
-    const sut = new AuthenticateUseCase(usersRepository)
+    it('should be able to authenticate', async () => {
+        await usersRepository.create({
+            name: 'John Doe',
+            email: 'johndoe@example.com',
+            password_hash: await hash('123456', 6),
+        })
 
-    await usersRepository.create({
-      name: 'John Doe',
-      email: 'johndoe@example.com',
-      password_hash: await hash('123456', 6),
+        const { user } = await sut.execute({
+            email: 'johndoe@example.com',
+            password: '123456',
+        })
+
+        expect(user.id).toEqual(expect.any(String))
     })
 
-    const { user } = await sut.execute({
-      email: 'johndoe@example.com',
-      password: '123456',
+    it('should not be able to authenticate with wrong email', async () => {
+        expect(() =>
+            sut.execute({
+                email: 'johndoe@example.com',
+                password: '123456',
+            }),
+        ).rejects.toBeInstanceOf(InvalidCredentialsError)
     })
 
-    expect(user.id).toEqual(expect.any(String))
-  })
+    it('should not be able to authenticate with wrong email', async () => {
+        await usersRepository.create({
+            name: 'John Doe',
+            email: 'johndoe@example.com',
+            password_hash: await hash('123456', 6),
+        })
 
-  it('should not be able to authenticate with wrong email', async () => {
-    const usersRepository = new InMemoryUsersRepository()
-    const sut = new AuthenticateUseCase(usersRepository)
-
-    expect(() =>
-      sut.execute({
-        email: 'johndoe@example.com',
-        password: '123456',
-      }),
-    ).rejects.toBeInstanceOf(InvalidCredentialsError)
-  })
-
-  it('should not be able to authenticate with wrong email', async () => {
-    const usersRepository = new InMemoryUsersRepository()
-    const sut = new AuthenticateUseCase(usersRepository)
-
-    await usersRepository.create({
-      name: 'John Doe',
-      email: 'johndoe@example.com',
-      password_hash: await hash('123456', 6),
+        expect(() =>
+            sut.execute({
+                email: 'johndoe@example.com',
+                password: '123123',
+            }),
+        ).rejects.toBeInstanceOf(InvalidCredentialsError)
     })
-
-    expect(() =>
-      sut.execute({
-        email: 'johndoe@example.com',
-        password: '123123',
-      }),
-    ).rejects.toBeInstanceOf(InvalidCredentialsError)
-  })
 })
--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,35 +1,42 @@
-import { expect, describe, it } from 'vitest'
+import { expect, describe, it, beforeEach } from 'vitest'
 import { RegisterUseCase } from './register'
 import { compare } from 'bcryptjs'
 import { InMemoryUsersRepository } from '../repositories/in-memory/in-memory-users-repository'
 import { UserAlreayExistsError } from './errors/user-already-exists-error'
 
+
+let usersRepository: InMemoryUsersRepository
+let sut: RegisterUseCase
+
+beforeEach(() => {
+  usersRepository = new InMemoryUsersRepository()
+  sut = new RegisterUseCase(usersRepository)
+
+})
+
+
 describe('Register Use Case', () => {
   it('should be able to register', async () => {
-    const usersRepository = new InMemoryUsersRepository()
-    const registerUseCase = new RegisterUseCase(usersRepository)
 
-    const { user } = await registerUseCase.execute({
+    const { user } = await sut.execute({
       name: 'John Doe',
       email: 'johndoe@example.com',
       password: '123456',
     })
-    
+
     expect(user.id).toEqual(expect.any(String))
 
 
   })
 
   it('should hash user password upon registration', async () => {
-    const usersRepository = new InMemoryUsersRepository()
-    const registerUseCase = new RegisterUseCase(usersRepository)
 
-    const { user } = await registerUseCase.execute({
+    const { user } = await sut.execute({
       name: 'John Doe',
       email: 'johndoe@example.com',
       password: '123456',
     })
-    
+
 
     const isPasswordCorrectlyHashed = await compare(
       '123456',
@@ -40,19 +47,17 @@ describe('Register Use Case', () => {
   })
 
   it('should not be able to register with same email twice', async () => {
-    const usersRepository = new InMemoryUsersRepository()
-    const registerUseCase = new RegisterUseCase(usersRepository)
 
     const email = 'johndoe@example.com'
 
-    await registerUseCase.execute({
+    await sut.execute({
       name: 'John Doe',
       email,
       password: '123456',
     })
 
-    await expect(() => 
-      registerUseCase.execute({
+    await expect(() =>
+      sut.execute({
         name: 'John Doe',
         email,
         password: '123456',
