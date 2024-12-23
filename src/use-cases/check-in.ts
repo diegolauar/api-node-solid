1d166b6db85c0864a5dcefc1945b80e93e314057
Diego Lauar <diego_pktd@hotmail.com>
2024-12-23 19:01:26 -0300

    Finalizando teste checkin

--- src/use-cases/check-in.ts
+++ src/use-cases/check-in.ts
@@ -2,28 +2,38 @@ import { CheckIn } from '@prisma/client'
 import { CheckInsRepository } from '../repositories/check-ins-repository'
 
 interface CheckInUseCaseRequest {
-  userId: string
-  gymId: string
+    userId: string
+    gymId: string
 }
 
 interface CheckInUseCaseResponse {
-  checkIn: CheckIn
+    checkIn: CheckIn
 }
 
 export class CheckInUseCase {
-  constructor(private checkInsRepository: CheckInsRepository) {}
+    constructor(private checkInsRepository: CheckInsRepository) { }
 
-  async execute({
-    userId,
-    gymId,
-  }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
-    const checkIn = await this.checkInsRepository.create({
-      gym_id: gymId,
-      user_id: userId,
-    })
+    async execute({
+        userId,
+        gymId,
+    }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
 
-    return {
-      checkIn,
+        const checkInOnSameDay = await this.checkInsRepository.findByUserIdOnDate(
+            userId,
+            new Date(),
+        )
+
+        if (checkInOnSameDay) {
+            throw new Error()
+        }
+
+        const checkIn = await this.checkInsRepository.create({
+            gym_id: gymId,
+            user_id: userId,
+        })
+
+        return {
+            checkIn,
+        }
     }
-  }
 }
