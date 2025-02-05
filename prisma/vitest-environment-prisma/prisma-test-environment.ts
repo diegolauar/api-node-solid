6626a901ff7d00ab26a08e99a8f64a903f9b32aa
Diego Lauar <diego_pktd@hotmail.com>
2025-02-05 16:11:45 -0300

    Controllers & Testes E2E - Teste E2E do perfil

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,36 +1,46 @@
 import 'dotenv/config'
 
 import { randomUUID } from 'node:crypto'
-import { execSync } from 'node:child_process'
 import { Environment } from 'vitest'
+import { execSync } from 'node:child_process'
 import { PrismaClient } from '@prisma/client'
+
+// postgresql://docker:docker@localhost:5432/api_solid_fev2024?schema=public
 
 const prisma = new PrismaClient()
 
 function generateDatabaseURL(schema: string) {
-    if (!process.env.DATABASE_URL) {
-        throw new Error('Please provide a DATABASE_URL environment variable.')
-    }
-    const url = new URL(process.env.DATABASE_URL)
-    url.searchParams.set('schema', schema)
-    return url.toString()
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL environment variable.')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
 }
 
 export default <Environment>{
-    name: 'prisma',
-    async setup() {
-        const schema = randomUUID()
-        const databaseURL = generateDatabaseURL(schema)
-        process.env.DATABASE_URL = databaseURL
-        execSync('npx prisma migrate deploy')
+  name: 'prisma',
+  transformMode: 'ssr',
+  async setup() {
+    const schema = randomUUID()
 
-        return {
-            async teardown() {
-                await prisma.$executeRawUnsafe(
-                    `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
-                )
-                await prisma.$disconnect()
-            },
-        }
-    },
+    const databaseURL = generateDatabaseURL(schema)
+
+    process.env.databaseURL = databaseURL
+
+    execSync('npx prisma migrate deploy')
+
+    return {
+      async teardown() {
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        )
+
+        await prisma.$disconnect()
+      },
+    }
+  },
 }
